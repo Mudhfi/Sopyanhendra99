32b1116188531ba6f5369629d6a9b8fa1b6bfe91
Mudhfi <mudfiupi@gmail.com>
2025-05-21 09:26:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("🦅 mantap men")
+st.title("🇪🇪 mantap men")
 st.write(
     "Madrid puasa gelar bubarin aja"
 )
-st.image("transparent_2024-08-03T14-11-31 .png",width=200)
+st.image("transparent_2024-08-03T14-11-31.png",width=200)
